2b90681c4309e4d352165727ea76925b3dbfbe7a
booldook <booldook@gmail.com>
2019-04-04 11:18:51 +0900

    DAILY DESIGN

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,14 +1,44 @@
-$.ajax({
-	type: "get",
-	url: "https://api.openweathermap.org/data/2.5/weather",
-	data: {
-		q: "seoul,kr",
-		appid: "02efdd64bdc14b279bc91d9247db4722",
-		units: "metric"
-	},
-	dataType: "json",
-	success: function(res) {
-		console.log(res);
-		//alert("현재 서울 기온은 : " + res.main.temp + "도 입니다.");
-	}
+// Ajax 에러
+function ajaxError(xhr, status, err) {
+	console.log(xhr);
+	console.log(status);
+	console.log(err);
+}
+
+// 도시id 가져오기
+getCity();
+function getCity() {
+	$.ajax({
+		type: "get",
+		url: "../json/city.json",
+		dataType: "json",
+		error: ajaxError,
+		success: function(res) {
+			var html = '';
+			var city = res.cities;
+			for(var i in city) {
+				html = '<option value="'+city[i].id+'">'+city[i].name+'</option>';
+				$("#city").append(html);
+			}
+		}
+	});
+}
+
+// 도시를 선택하면 현재날씨 가져오기
+$("#city").change(function(){
+	$.ajax({
+		type: "get",
+		url: "https://api.openweathermap.org/data/2.5/weather",
+		data: {
+			id: $(this).val(),
+			appid: "02efdd64bdc14b279bc91d9247db4722",
+			units: "metric"
+		},
+		dataType: "json",
+		error: ajaxError,
+		success: getDaily
+	});
 });
+function getDaily(res) {
+	console.log(res);
+}
